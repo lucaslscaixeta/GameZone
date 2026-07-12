27888b8491f18580ac14e0911cbba49a2618d98f
lucaslscaixeta <lucas1000jog@gmail.com>
2026-07-12 14:09:20 -0300

    test(minesweeper): add Cypress acceptance tests

--- Games/Minesweeper/cypress.config.js
+++ Games/Minesweeper/cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    baseUrl: "http://127.0.0.1:5500/Games/Minesweeper",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
